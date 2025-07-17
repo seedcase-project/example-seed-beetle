a293ebc50d666b1d773028bf020d12bd919a2b11
signekb <signekb@clin.au.dk>
2025-07-17 14:41:01 +0200

    refactor: :recycle: update `main.py` to write to `datapackage.json` and write to it

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import seedcase_sprout as sp
 
+from scripts.properties import properties
+
 
 def main():
     """Pipeline for creating the seed beetle Data Package."""
     # Create the properties script in the default location.
     sp.create_properties_script()
+    # Write properties to `datapackage.json`.
+    sp.write_properties(properties=properties)
 
 
 if __name__ == "__main__":
